3c2902f5f3f20bacf6fa6d04a72b7761fdb03457
Nate <nathanl3@uci.edu>
2017-02-04 16:16:34 -0800

    Changed functions

--- hw1/parallel-mergesort.cc
+++ hw1/parallel-mergesort.cc
@@ -9,12 +9,12 @@
 #include <stdlib.h>
 #include <omp.h>
 #include "sort.hh"
-keytype* merge(keytype* A,keytype* B  );
+keytype* merge(keytype* A,keytype* B , int SizeA, int SizeB );
 int binarySearch(keytype* A, int vertex);
 
 keytype* parallelSort (int N, keytype* A)
 {	
-	if( N < 1){
+	if( N < 2){
 	  return A;
 	}
 	keytype* temp = newKeys(N/2);
@@ -28,40 +28,33 @@ keytype* parallelSort (int N, keytype* A)
 	
 	int x = (N/2) ;
 	int y = 0;
+	int x2 = N - x;
 	for(int i = x ; i < N ; i ++){
 		temp2[y] = A[i];
-		y++;
-		
-		
+		y++;	
 	}
 //	#pragma omp parallel 
 //	{
-		temp3 = parallelSort( N/2, temp);
+		temp3 = parallelSort( x, temp);
 //	}	
 //	#pragma omp parallel 
 //	{
-		temp4 = parallelSort( N/2, temp2);
+		temp4 = parallelSort( x, temp2);
 //	}
-	
 //	#pragma omp barrier
-	return merge(temp,temp4);
+	return merge(temp3,temp4,x,x2);
 }
 
-keytype* merge(keytype* A,keytype* B ){
+keytype* merge(keytype* A,keytype* B,int SizeA,int SizeB ){
 	int V1;
-	int x1 = sizeof(A) / sizeof(int);
-	int x2 = x1 /2;
-	int y1 = sizeof(B) / sizeof(int);
-	int y2 = y1 /2;
-	int s1,s2,s3,s4;
+	int size = SizeA + SizeB;
+	keytype* Small = newKeys(2);
+	int x1 = SizeA /2;
 	int temp = 0;
 	int temp2 = 0;
-	int size;
-	size = x1 + y1;
-	keytype* Small = newKeys(2);
-	
 	if( size <= 2){ //if A and B are one
-		if (A[0] > B[0]){
+		
+		if (A[0] > B[0]){			
 			Small[0] = B[0];
 			Small[1] = A[0];
 		}
@@ -72,21 +65,22 @@ keytype* merge(keytype* A,keytype* B ){
 		return Small;
 	}
 	
-	
-	V1 = A[x2];
+	keytype* Test = newKeys(size);
+	V1 = A[x1];
 	int k = binarySearch(B, V1);
-	printf("%d",k);
-	keytype* C1 = newKeys(x2);
-	keytype* C2 = newKeys(x2);
-	keytype* D1 = newKeys(y2);
-	keytype* D2 = newKeys(y2);
+	int k2 = SizeB - k;
+	keytype* C1 = newKeys(x1);
+	keytype* C2 = newKeys(x1);
+	keytype* D1 = newKeys(k);
+	keytype* D2 = newKeys(k2);
+	
 	//Partition into C
-	for(int i = 0 ; i < x2 ; i++ ){ //C1
+	for(int i = 0 ; i < x1 ; i++ ){ //C1
 		C1[i] = A[i];
 		
 	}
 	
-	for(int i = x2 ; i < x1 ; i++ ){ //C2
+	for(int i = x1 ; i < SizeA ; i++ ){ //C2
 		C1[temp] = A[i];
 		temp++;
 	}
@@ -96,47 +90,20 @@ keytype* merge(keytype* A,keytype* B ){
 		D1[i] = B[i];
 	}
 	
-	for(int i = k ; i < y1  ; i ++){ //D2
+	for(int i = k ; i < SizeB  ; i ++){ //D2
 		D2[temp2] = B[i];
 		temp2++;
 	}
+	int size3 = k + x1; //E1
+	int size4 = SizeA + k2; //E2
+	keytype* E1 = newKeys(size3);
+	keytype* E2 = newKeys(size4);
+	E1 = merge(C1,D1,x1,k); //E1
+	E2 = merge(C2,D2,x1,k2); //E2
 	
-	s1 = sizeof(C1) / sizeof(int);
-	s2 = sizeof(C2) / sizeof(int);
-	s3 = sizeof(D1) / sizeof(int);
-	s4 = sizeof(D2) / sizeof(int);
-	keytype* E1 = newKeys(s1+s3);
-	keytype* E2 = newKeys(s2+s4);
-	printf("Test");
-	E1 = merge(C1,D1); //E1
-	
-	E2 = merge(C2,D2); //E2
-	
-	size = x1 + y1;
+	size = SizeA + SizeB;
 	keytype* Final = newKeys(size);
-	x1 = sizeof(A) / sizeof(int);
-	y1 = sizeof(B) / sizeof(int);
-	for(int i = 0 ; i < size ; i++){
-		
-		if( i < x1 ){
-			Final[i] = E1[i];
 
-		}
-		
-		else if (i == x1){
-			Final[i] = V1;
-			i++;			
-		}
-		
-		else{
-			Final[i] = E2[i];
-		}
-	}
-	
-/* 	for (int i = 0; i < size ; i++){
-		printf("%d",Final[i]);
-	}  */
-	
 	return Final;
 }
 
