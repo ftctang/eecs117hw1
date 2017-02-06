991851f6af29c6383de85d968ca9af9ce87bdfc4
Nate <nathanl3@uci.edu>
2017-02-05 20:56:16 -0800

    Now everything is done.

--- hw1/parallel-mergesort.cc
+++ hw1/parallel-mergesort.cc
@@ -12,7 +12,6 @@
 #include <string.h>
 
 void  mergesort(keytype* A , int p , int r,keytype* Init , int threads);
-void  merge(keytype* A,int p, int q, int r ,keytype* Init,int threads );
 void  pmerge(keytype* A, keytype* Init , int p1,int r1, int p2,int r2 , int p3, int threads);
 void smergesort(keytype* A, int p, int r, keytype* Init);
 void smerge( keytype* A, int p, int q, int r, keytype* Init);
@@ -20,43 +19,50 @@ int binarySearch(keytype* Init, int vertex , int p, int r);
 
 void parallelSort (int N, keytype* A) //Main Function
 {	
-	omp_set_num_threads(N-1);
-	int tcount = omp_get_num_threads();
+	
 	keytype* Init = newKeys(N);
-	#pragma omptask parallel
+	#pragma omp parallel //Sets parallel region 
 	{
-	mergesort(A, 0, N-1,Init, N/tcount);	
+		int tcount = omp_get_num_threads(); //Thread Count
+		
+		#pragma omp single // So function is only executed once
+		{
+		mergesort(A, 0, N-1,Init, N/tcount);	//Start mergesort
+		}
 	}
 	
 	free (Init); //Release 
 }
 
 void  mergesort(keytype* A , int p , int r, keytype* Init, int threads){ //MergeSort
-	if ( (r-p + 1) <= threads) {
-		smergesort(A,  p, r, Init); //Correction
-		return;
-	}	
-	
-	int mid = p + (p+r)/2;
-	#pragma omp task
-	mergesort( A, p,mid,Init,threads);
-	mergesort( A,mid+1, r,Init,threads);
-	#pragma omp taskwait
-	merge( A ,p,mid,r,Init,threads);
-	memcpy (A + p, Init + p, (r-p+1) * sizeof(keytype)); //Copy Init into A
-	
-}
+//Follow parallel Mergesort psuedo code based on Introduction to Algorithms 3rd edition 
 
-void merge(keytype* A,int p, int q, int r ,keytype* Init,int threads){//Merge
-	pmerge(A,Init ,p ,q , q+1,r,p,threads);// Call parallel merge
+	if ((r - p + 1) <= threads) { // This is for threads exceeds the array size if not added reaches seg fault	
+			smergesort(A, p, r, Init);  
+		return;
+		}
+	
+	int mid = p + (r-p)/2; // Mid Point
+	
+	#pragma omp task//Spawn
+	{
+	mergesort( A, p,mid,Init,threads); // Recursive Mergesort for left side
+	}
+	mergesort( A,mid+1, r,Init,threads); // Recursive Mergesort for right side
+	#pragma omp taskwait //Sync
+	
+	pmerge( A ,Init,p,mid,mid+1,r,p,threads);//Merges both sides together
+	
+	memcpy (A + p, Init + p, (r-p+1) * sizeof(keytype)); //Copy Init into A 
+	
 }
 
 void pmerge(keytype* A, keytype* Init , int p1,int r1, int p2,int r2 , int p3, int threads ){// Parallel Merge
+	//Follow parallel Mergepsuedo code based on Introduction to Algorithms 3rd edition 
+	int n1 = r1 - p1 + 1; // Sets Partition 1 length
+	int n2 = r2 - p2 + 1; // Sets Partition 2 length
 	
-	int n1 = r1 - p1 + 1;
-	int n2 = r2 - p2 + 1;
-	
-	if(n1 + n2 <= threads){
+	if(n1 + n2 <= threads){ //Threads exceed size
 		int x = p1;
 		int y = p2;
 		int z = p3;
@@ -79,7 +85,7 @@ void pmerge(keytype* A, keytype* Init , int p1,int r1, int p2,int r2 , int p3, i
 		}
 		
 		while(y <= r2){  //Values left in A[p2,r2]
-			Init[z] = A[z];
+			Init[z] = A[y];
 			y++;
 			z++;
 		}
@@ -89,13 +95,13 @@ void pmerge(keytype* A, keytype* Init , int p1,int r1, int p2,int r2 , int p3, i
 	if(n1 < n2){ //ensure n1 <= n2
 		int temp = p2;
 		p2 = p1;
-		p1 = temp;
+		p1 = temp; //Exchange p1 with p2
 		temp = r2;
 		r2 = r1;
-		r1 = temp;
+		r1 = temp; //Exchange r1 with r2 
 		temp = n2;
 		n2 = n1;
-		n1 = temp;
+		n1 = temp; //Exchange n1 with n2 
 	}
 	
 	if(n1 == 0){ //Empty
@@ -103,15 +109,18 @@ void pmerge(keytype* A, keytype* Init , int p1,int r1, int p2,int r2 , int p3, i
 	}
 	
 	
-	int q1 = (p1 + r1) /2;
+	int q1 = (p1 + r1) /2; // Mid point1
 	int vertex = A[q1];
-	int q2 = binarySearch(A,vertex , p2,r2);
-	int q3 = p3 + (q1 - p1) + (q2 -p2);
+	int q2 = binarySearch(A,vertex , p2,r2); // Mid Point 2
+	int q3 = p3 + (q1 - p1) + (q2 -p2); //Mid point for Init
 	Init[q3] = A[q1];
-	#pragma omp task
+	
+	#pragma omp task // Spawn
+	{
 	pmerge(A,Init,p1,q1-1,p2,q2-1,p3,threads);
+	}
 	pmerge(A,Init,q1+1,r1,q2,r2,q3+1,threads);
-	#pragma omp taskwait	
+	#pragma omp taskwait	//Sync
 	
 }
 
@@ -143,7 +152,7 @@ int binarySearch(keytype* A, int vertex , int p, int r) //Search for Index
 }
 
 void smergesort(keytype* A, int p, int r, keytype* Init){ // Base case sorting
-	
+
 	if(p < r){
 		int q = (p+r)/2;
 		smergesort(A, p, q, Init);
